b462e4f8f7c7fcd2533d05743b15ea8f7a7abc84
Justin Guerra <justinguerra@Justins-MacBook-Air.local>
2015-11-19 17:40:12 -0600

    added somthing

---
+++ jetBox/JetBox/Movie.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/JetBoxHome.Master" AutoEventWireup="true" CodeBehind="Movie.aspx.cs" Inherits="JetBox.WebForm11" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <p>
+        Home&nbsp;&nbsp;&nbsp;
+    </p>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
+    <p>
+        Movies</p>
+</asp:Content>
+<asp:Content ID="Content4" ContentPlaceHolderID="ContentPlaceHolder3" runat="server">
+    <p>
+        Sign Out</p>
+</asp:Content>
+<asp:Content ID="Content5" ContentPlaceHolderID="ContentPlaceHolder4" runat="server">
+    <p>
+        Cart</p>
+</asp:Content>
+<asp:Content ID="Content6" ContentPlaceHolderID="MainContentArea" runat="server">
+    <p>Movie image shows up here</p>
+    <br />
+    <p>Movie Description</p>
+    <br />
+    <p>Movie Rating</p>
+    <br />
+    <p>User Movie Rating</p>
+
+</asp:Content>
--- jetBox/JetBox/signup.aspx.cs
+++ jetBox/JetBox/signup.aspx.cs
@@ -18,7 +18,7 @@ namespace JetBox
 
         protected void ButtonSignin_Click(object sender, EventArgs e)
         {
-            if (txtEmail.Text == "" || txtUserID.Text == "")
+            if (txtEmail.Text == "")
             {
                 if (txtEmail.Text == "")
                 {
@@ -26,24 +26,18 @@ namespace JetBox
                     LabelEmailWarning.Visible = true;
                 }
 
-                if (txtUserID.Text == "")
-                {
-                    LabelIDWarning.Text = "*Required";
-                    LabelIDWarning.Visible = true;
-                }
-
             }
             else
             {
 
-                string value = txtUserID.Text;
+            
                 /* 
                 SqlConnection db = new SqlConnection(SqlDataSource1.ConnectionString);
 
                 SqlCommand cmd = new SqlCommand();
 
                 cmd.CommandType = System.Data.CommandType.Text;
-                cmd.CommandText = "SELECT COUNT(*) FROM [User] WHERE ([User_Username] = '" + value + "')";
+                cmd.CommandText = "SELECT COUNT(*) FROM [User] WHERE ([User_Email] = '" + value + "')";
                 cmd.Connection = db;
 
                 db.Open();
@@ -56,27 +50,26 @@ namespace JetBox
                     //int cat = (int)cmd.ExecuteScalar();
                     if (cat == 0)
                     {
-                        LabelIDWarning.Text = "Success!";
-                        LabelIDWarning.Visible = true;
-                        LabelEmailWarning.Visible = false;
+                        LabelEmailWarning.Text = "Success!";
+                        LabelEmailWarning.Visible = true;
 
-                        Session["uName"] = txtUserID.Text;
+                       
                         Session["uEmail"] = txtEmail.Text;
-                        Response.Redirect("/SignUp.aspx");
+                        Response.Redirect("/SignUp2.aspx");
 
                     }
                     else
                     {
-                        LabelIDWarning.Text = "Not Unique!";
-                        LabelIDWarning.Visible = true;
+                        LabelEmailWarning.Text = "Not Unique!";
+                        LabelEmailWarning.Visible = true;
                     }
 
                 }
                 catch
                 {
 
-                    LabelIDWarning.Text = "Error!";
-                    LabelIDWarning.Visible = true;
+                    LabelEmailWarning.Text = "Error!";
+                    LabelEmailWarning.Visible = true;
 
                 }
                 finally
